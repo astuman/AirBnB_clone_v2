22bd720cf13997dadefa951eb6650167388b55bc
Astewul <Astewul>
2022-10-12 23:44:25 +0300

    fix my codes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,12 +12,11 @@ class FileStorage:
         """Returns a dictionary of models currently in storage"""
         if cls is None:
             return FileStorage.__objects
-
-        objects = {}
-        for key, value in FileStorage.__objects.items():
-            objects.update({key: value})
-
-        return objects
+        my_dict = {}
+        for key, val in FileStorage.__objects.items():
+            if isinstance(val, cls):
+                my_dict[key] = val
+        return my_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -52,15 +51,23 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        if obj:
-            name = obj.__class__.__name__
-            id = obj.id
-            key = ".".join([name, id])
-            if key in FileStorage.__objects:
-                del FileStorage.__objects[key]
-                self.save()
+        """public instance method to delete obj from __objects
+        if it’s inside
+        """
+        if obj is None:
+            return
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        if key in FileStorage.__objects:
+            del FileStorage.__objects[key]
+            self.save()
+
+    def close(self):
+        """Public method to call reload for deserializing the
+        JSON file.
+        """
+        self.reload()
