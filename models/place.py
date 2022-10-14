b8a994019d2d796e76f6ef7dbb1823d060c69db8
Astewul <loloalex1992@gmail.com>
2022-10-15 01:18:36 +0300

    task completed

--- models/place.py
+++ models/place.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base, Amenity
 from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
