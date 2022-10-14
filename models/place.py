ca8b92f25fc035ae713da8364eb40b3043239618
Astewul <loloalex1992@gmail.com>
2022-10-15 01:27:17 +0300

    task completed

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from AirBnB_clone_v2.models.amenity import Amenity
+from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
