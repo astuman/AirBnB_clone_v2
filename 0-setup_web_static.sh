6ad7fcbcf631e480731a0d527bebeef6f8593870
Astewul <loloalex1992@gmail.com>
2022-10-27 16:27:24 +0300

    commit my tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "Hellow World!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 
