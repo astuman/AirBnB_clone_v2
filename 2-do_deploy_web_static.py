adf076355f5c44275c91846cc20783793790b523
Astewul <loloalex1992@gmail.com>
2022-10-27 22:47:38 +0300

    commit task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -43,5 +43,3 @@ def do_deploy(archive_path):
         # web server, linked to the new version of your code
         # (/data/web_static/releases/<archive filename without extension>)
         return True
-    except:
-        return False
