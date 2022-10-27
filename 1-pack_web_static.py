d360fbca4010e15c38175177a9a214be8b979a22
Astewul <loloalex1992@gmail.com>
2022-10-27 21:55:28 +0300

    commit task 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import put
 from fabric.api import run
 
 
-env.hosts = ["54.173.84.88", "34.204.61.191	"]
+env.hosts = ["54.173.84.88", "34.204.61.191"]
 
 
 def do_deploy(archive_path):
