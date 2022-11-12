63f1724c0cba65f0b17f30614cbc4c6c3ad04203
Astewul <loloalex1992@gmail.com>
2022-11-12 19:00:04 +0300

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,6 +9,6 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_world():
     """Returns Hello HBNB"""
-    return 'Hello HBNB!'
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=500)
+    return "Hello HBNB!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=500)
