4337814de7d2068e8dd7dcb2d685150ac7caa129
Astewul <loloalex1992@gmail.com>
2022-11-13 20:47:07 +0300

    list states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,67 +1,27 @@
 #!/usr/bin/python3
-""" 5. Add fifth view func that displays HTML page if n is int """
-from models import FileStorage
-from flask import Flask
-from flask import render_template
+"""
+starts a Flask web application
+"""
 
+
+from flask import Flask, render_template
+from models import *
+from models import storage
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route('/')
-def hello_world():
-    """ Returns some text. """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb')
-def hello():
-    """ Return other text. """
-    return 'HBNB'
-
-
-@app.route('/c/<text>')
-def c_text(text):
-    """ replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
-
-
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ replace more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
-
-
-@app.route('/number/<int:n>')
-def number_text(n):
-    """ replace with int only if given int. """
-    n = str(n)
-    return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>')
-def html_num(n):
-    """ display html if n is int. """
-    n = str(n)
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def odd_or_even(n):
-    """ display different page depending on var given odd or even. """
-    return render_template('6-number_odd_or_even.html', n=n)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """displays a HTML page of state objects present in DBstorage
-    message on page changes depending on value passed"""
-    return render_template('7-states_list.html', states=FileStorage.all("State"))
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
