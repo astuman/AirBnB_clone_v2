5cc4a37d42ba6896732e3743756d3faed4b6a70e
Astewul <loloalex1992@gmail.com>
2022-11-14 22:08:25 +0300

    commit last task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,13 +3,9 @@
 starts a Flask web application
 """
 
-
 from flask import Flask, render_template
-
 from models import *
-
-# from models import storage
-
+from models import storage
 app = Flask(__name__)
 
 
